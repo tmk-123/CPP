344c237349f117d847b8873b6109c20fb21fe061
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-12-11 18:52:32 +0700

    Quản lý bán hàng - 3

--- CPP0806.cpp
+++ CPP0806.cpp
@@ -2,116 +2,89 @@
 
 using namespace std;
 
-class KhachHang;
-class MatHang;
-
-// không dùng unordered_map được bởi vì cần có constructor
-map<int, KhachHang> KH;
-map<int, MatHang> MH;
-
 class KhachHang {
 public:
-    int code;
-    string name, gender, birthday, address;
+    string clientCode, clientName, gender, bd, address;
 };
 
 class MatHang {
 public:
-    int code;
-    string name, unit;
-    long long buy, sell;
+    string productCode, productName, unit;
+    int buy, sell; 
 };
 
-string formatStr(string s) {
-    stringstream ss(s);
-    string word, res;
+class HoaDon {
+public:
+    string invoiceCode;
+    KhachHang kh;
+    MatHang mh;
+    int quantity;
+};
 
-    while (ss >> word) {
-        res += word;
-        res += " ";
-    }
-
-    res.pop_back();
-    return res;
-}
-
-void get_KH() {
+int main() {
     fstream fs;
     fs.open("KH.in");
-
     int n;
     fs >> n;
-
+    vector<KhachHang> KH;
     for (int i = 0; i < n; i++) {
-        int code = i + 1;
-        string name, gender, birthday, address;
-
-        getline(fs >> ws, name);
-        getline(fs >> ws, gender);
-        getline(fs >> ws, birthday);
-        getline(fs >> ws, address);
-
-        KH[i + 1] = {code, formatStr(name), formatStr(gender), formatStr(birthday), formatStr(address)}; 
+        KhachHang a;
+        getline(fs >> ws, a.clientName);
+        getline(fs >> ws, a.gender);
+        getline(fs >> ws, a.bd);
+        getline(fs >> ws, a.address);
+        string s = to_string(i + 1);
+        a.clientCode = "KH" + string(3 - s.length(), '0') + s;
+        KH.push_back(a);
     }
-    
     fs.close();
-}
 
-void get_MH() {
-    fstream fs;
     fs.open("MH.in");
-
-    int n;
-    fs >> n;
-
-    for (int i = 0; i < n; i++) {
-        int code = i + 1;
-        string name, unit;
-        long long buy, sell;
-
-        getline(fs >> ws, name);
-        getline(fs >> ws, unit);
-
-        fs >> buy >> sell;
-
-        MH[i + 1] = {code, formatStr(name), formatStr(unit), buy, sell};
+    int m;
+    fs >> m;
+    vector<MatHang> MH;
+    for (int i = 0; i < m; i++) {
+        MatHang a;
+        getline(fs >> ws, a.productName);
+        getline(fs >> ws, a.unit);
+        fs >> a.buy >> a.sell;
+        string s = to_string(i + 1);
+        a.productCode = "MH" + string(3 - s.length(), '0') + s;
+        MH.push_back(a);
     }
-
     fs.close();
-}
 
-void show_HD() {
-    fstream fs;
     fs.open("HD.in");
+    int k;
+    fs >> k;
 
-    int n;
-    fs >> n;
+    vector<HoaDon> HD;
+    for (int i = 0; i < k; i++) {
+        HoaDon a;
+        fs >> a.kh.clientCode >> a.mh.productCode >> a.quantity;
+        string s = to_string(i + 1);
+        a.invoiceCode = "HD" + string(3 - s.length(), '0') + s;
 
-    string clientCode, productCode;
-    int quantity;
+        for (auto x : KH) {
+            if (x.clientCode == a.kh.clientCode) {
+                a.kh = x;
+                break;
+            }
+        }
 
-    for (int i = 0; i < n; i++) {
-        cout << "HD" << setfill('0') << setw(3) << i + 1 << " ";
-
-        fs >> clientCode >> productCode >> quantity;
-
-        // &clientCode[2] là con trỏ trỏ đến vị trí thứ 2 của chuỗi, nghĩa là lấy từ 2 về cuối 
-        int khIndex = stoi(&clientCode[2]);
-        int mhIndex = stoi(&productCode[2]);
-
-        cout << KH[khIndex].name << " "
-            << KH[khIndex].address << " "
-            << MH[mhIndex].name << " "
-            << MH[mhIndex].unit << " "
-            << MH[mhIndex].buy << " "
-            << MH[mhIndex].sell << " "
-            << quantity << " "
-            << quantity * MH[mhIndex].sell << endl;
+        for (auto x : MH) {
+            if (x.productCode == a.mh.productCode) {
+                a.mh = x;
+                break;
+            }
+        }
+        HD.push_back(a);
     }
     fs.close();
-}
-int main () {
-    get_KH();
-    get_MH();
-    show_HD();
+
+    for (int i = 0; i < k; i++) {
+        cout << HD[i].invoiceCode << " " << HD[i].kh.clientName << " " << HD[i].kh.address << " " << HD[i].mh.productName
+             << " " << HD[i].mh.unit << " " << HD[i].mh.buy << " " << HD[i].mh.sell << " " << HD[i].quantity << " " 
+             << HD[i].quantity * HD[i].mh.sell << endl;
+    }
 }
